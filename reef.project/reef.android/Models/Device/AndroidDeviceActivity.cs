a6a1b6019855ea8de6321c7c1fa2ebad80f6f232
Wednesday Wolf <Contact@wednesdaywolf.com>
2021-04-28 13:15:20 -0700

    Had to migrate do to Mono Update

---
+++ reef.project/reef.android/Models/Device/AndroidDeviceActivity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Content.PM;
+using reef.shared.Models.Device;
+
+namespace reef.android {
+  public class AndroidDeviceActivity : IDeviceActivity {
+    public AndroidDeviceActivity() {
+    }
+
+    public void Get() {
+      IList<ApplicationInfo> androidAppInfo = PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
+      IList<AppInfo> appInfo = new List<AppInfo>();
+      for (ApplicationInfo info : androidAppInfo) {
+        appInfo.Add(new AppInfo(info.Name, info.PackageName));
+      }
+      return appInfo;
+    }
+}

3bf303529d3677f54026d5ac3b8c2c67a46caa3b
Wednesday Wolf <Contact@wednesdaywolf.com>
2021-04-28 13:22:14 -0700

    Commenting out buggy code in AndroidDevice

--- reef.project/reef.android/Models/Device/AndroidDeviceActivity.cs
+++ reef.project/reef.android/Models/Device/AndroidDeviceActivity.cs
@@ -7,13 +7,13 @@ namespace reef.android {
   public class AndroidDeviceActivity : IDeviceActivity {
     public AndroidDeviceActivity() {
     }
-
     public void Get() {
-      IList<ApplicationInfo> androidAppInfo = PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
-      IList<AppInfo> appInfo = new List<AppInfo>();
-      for (ApplicationInfo info : androidAppInfo) {
-        appInfo.Add(new AppInfo(info.Name, info.PackageName));
-      }
-      return appInfo;
+      //IList<ApplicationInfo> androidAppInfo = PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
+      //IList<AppInfo> appInfo = new List<AppInfo>();
+      //for (ApplicationInfo info : androidAppInfo) {
+      //  appInfo.Add(new AppInfo(info.Name, info.PackageName));
+      //}
+      //return appInfo;
     }
+  }
 }
--- reef.project/reef.shared/Models/Device/InstalledApps.cs
+++ reef.project/reef.shared/Models/Device/InstalledApps.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace reef.shared.Models.Device {
     public abstract class InstalledApps {
         /// <summary>
         /// Returns a list containing information about the apps installed on the device.
         /// </summary>
-        public abstract Collections.Generic.IList<AppInfo> Get();
+        public abstract IList<AppInfo> Get();
     }
 }

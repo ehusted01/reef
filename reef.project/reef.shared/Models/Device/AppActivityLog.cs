87ca1dcce823dbe799ca29edab6e11b4eaf502fa
Ethan Husted <ethanhusted11@gmail.com>
2021-04-30 16:57:40 -0700

    Partially implemented AppActivityLog

--- reef.project/reef.shared/Models/Device/AppActivityLog.cs
+++ reef.project/reef.shared/Models/Device/AppActivityLog.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 
 namespace reef.shared.Models.Device {
     public class AppActivityLog {
+        private static readonly int LOG_LENGTH = 30;
+        private double[] Log;
+        private int CurrPos;
+
+        public AppActivityLog() {
+            Log = new double[LOG_LENGTH];
+            CurrPos = 0;
+        }
         public void LogUsage(double hours) {
-            throw new NotImplementedException();
+            CurrPos = (CurrPos + 1) % LOG_LENGTH;
+            Log[CurrPos] = hours;
+        }
+        public double GetUsage() {
+            return Log[CurrPos];
         }
     }
 }

8d2881255564642d3406fb8ef21a03c48a069ba7
Wednesday Wolf <Contact@wednesdaywolf.com>
2021-04-30 11:35:18 -0700

    Adding savefile example

---
+++ reef.project/reef.shared/Models/SaveFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reef.shared.Models {
+  /// <summary>
+  /// The save file for saving the state of the world
+  /// </summary>
+  public struct SaveFile {
+    public List<string> ExampleField;
+  }
+}
---
+++ reef.project/reef.shared/Models/User.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace reef.shared.Models {
+  public class User {
+    public User() {
+    }
+  }
+}

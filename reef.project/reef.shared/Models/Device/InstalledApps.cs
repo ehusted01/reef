9e486464d57da353cb9bd10c68226114d5d57c39
Ethan Husted <ethanhusted11@gmail.com>
2021-04-30 15:41:58 -0700

    Added IsInstalled() function to InstalledApps

--- reef.project/reef.shared/Models/Device/InstalledApps.cs
+++ reef.project/reef.shared/Models/Device/InstalledApps.cs
@@ -7,5 +7,11 @@ namespace reef.shared.Models.Device {
         /// Returns a list containing information about the apps installed on the device.
         /// </summary>
         public abstract IList<AppInfo> Get();
+        /// <summary>
+        /// Return true if the app, info, is installed. False otherwise.
+        /// </summary>
+        /// <param name="info">the app that is checked</param>
+        /// <returns>True if info is installed, False otherwise</returns>
+        public abstract bool IsInstalled(AppInfo info);
     }
 }

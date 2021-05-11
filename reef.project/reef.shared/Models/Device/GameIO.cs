c8c2ffadac5d7f48a14aa1a64794f04bcdf8f16b
Wednesday Wolf <Contact@wednesdaywolf.com>
2021-05-11 12:36:10 -0700

    Adding fishmanager / io stuff back in

--- reef.project/reef.shared/Models/Device/GameIO.cs
+++ reef.project/reef.shared/Models/Device/GameIO.cs
@@ -3,10 +3,8 @@ using System.IO;
 
 
 // DO NOT DELETE THIS
-namespace reef.shared.Models.Device
-{
-    public abstract class GameIO
-    {
+namespace reef.shared.Models.Device {
+    public abstract class GameIO {
         public abstract StreamReader ReadLocalJsonFile(string filePath);
     }
 }

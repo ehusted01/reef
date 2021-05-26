009abccf9ba46d72b80cfdb88ae7b46277279cb7
Ethan Husted <ethanhusted11@gmail.com>
2021-05-26 16:55:18 -0700

    FishUpdateScheduler v1

---
+++ reef.project/reef.android/AndroidFishUpdateScheduler.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.App.Job;
+using Android.Content;
+using reef.shared;
+using System;
+
+namespace reef.android {
+    class AndroidFishUpdateScheduler : FishUpdateScheduler {
+        private static readonly long JOB_INTERVAL = 86400000;
+        public void Schedule() {
+            Java.Lang.Class service = Java.Lang.Class.FromType(typeof(FishUpdateService));
+            JobInfo.Builder jobBuilder = new JobInfo.Builder(314, new ComponentName(Application.Context, service));
+            JobInfo fishUpdate = jobBuilder.SetBackoffCriteria(0, BackoffPolicy.Linear).SetPeriodic(JOB_INTERVAL).SetPersisted(true).Build();
+
+            JobScheduler jobSched = (JobScheduler)Application.Context.GetSystemService(Context.JobSchedulerService);
+            jobSched.Schedule(fishUpdate);
+        }
+    }
+}
---
+++ reef.project/reef.shared/FishUpdateScheduler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reef.shared {
+    public interface FishUpdateScheduler {
+        /// <summary>
+        /// Schedules the FishUpdate task.
+        /// </summary>
+        public void Schedule();
+    }
+}
